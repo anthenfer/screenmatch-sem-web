c0f1ad27d2068f3f49d877a8bb5905409ee38ae2
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2025-05-27 22:33:19 -0300

    Adicionando API de tradução para sinopse + dependencia de banco de dados + removendo token CHATGPT API

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("Digite sua chave token aqui");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
